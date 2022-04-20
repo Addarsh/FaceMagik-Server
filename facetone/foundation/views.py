c916d9e72eabf0ec555303d687e8446b6af62614
Addarsh <addarsh.cherokie@gmail.com>
2022-04-19 22:19:48 -0700

    Using contour points to create face mask info
    Will update client to not send masks in a future change

--- facetone/foundation/views.py
+++ facetone/foundation/views.py
@@ -48,15 +48,10 @@ class Session(APIView):
     def post(self, request):
         serializer = SessionSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        Session.load_maskrcnn_model()
 
         session_id = serializer.get_session_id()
         image_name = serializer.get_image_name()
         image = Session.get_image(request.data["image"])
-        face_mask = Session.get_image(request.data["face_mask"])
-        mouth_mask = Session.get_image(request.data["mouth_mask"])
-        left_eye_mask = Session.get_image(request.data["left_eye_mask"])
-        right_eye_mask = Session.get_image(request.data["right_eye_mask"])
         nose_middle_point = request.data["nose_middle_point"]
         face_till_nose_end_contour_points = request.data["face_till_nose_end_contour_points"]
         mouth_without_lips_contour_points = request.data["mouth_without_lips_contour_points"]
@@ -68,10 +63,6 @@ class Session(APIView):
 
         # For debugging purposes only. In production, we would upload the image to blob store like Amazon S3.
         Session.save_image_to_file(image_name, image)
-        Session.save_image_to_file("test_face_mask.png", face_mask)
-        Session.save_image_to_file("test_mouth_mask.png", mouth_mask)
-        Session.save_image_to_file("left_eye_mask.png", left_eye_mask)
-        Session.save_image_to_file("right_eye_mask.png", right_eye_mask)
         Session.save_nose_middle_point_to_file(nose_middle_point)
         Session.save_contour_points_to_file("face_till_nose_end_contour_points.txt", face_till_nose_end_contour_points)
         Session.save_contour_points_to_file("mouth_without_lips_contour_points.txt", mouth_without_lips_contour_points)
@@ -82,8 +73,20 @@ class Session(APIView):
         Session.save_contour_points_to_file("right_eyebrow_contour_points.txt", right_eyebrow_contour_points)
 
         # Detect lighting conditions.
-        face_mask_info = Session.create_face_mask_info(face_mask, mouth_mask, left_eye_mask, right_eye_mask,
-                                                       nose_middle_point)
+        face_mask_info = Session.create_face_mask_info_from_contour_points(image=image,
+                                                                           face_till_nose_end_cpts=np.array(
+                                                                               face_till_nose_end_contour_points),
+                                                                           left_eye_cpts=np.array(
+                                                                               left_eye_contour_points),
+                                                                           right_eye_cpts=np.array(
+                                                                               right_eye_contour_points),
+                                                                           left_eyebrow_cpts=np.array(
+                                                                               left_eyebrow_contour_points),
+                                                                           right_eyebrow_cpts=np.array(
+                                                                               right_eyebrow_contour_points),
+                                                                           mouth_without_lips_cpts=
+                                                                           np.array(mouth_without_lips_contour_points),
+                                                                           nose_middle_point=nose_middle_point)
         skin_tone_analyzer = SkinToneAnalyzer(maskrcnn_model, Session.get_skin_detection_config(image), face_mask_info)
 
         try:
@@ -140,6 +143,23 @@ class Session(APIView):
 
         except SkinToneDetectionSession.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND, data=create_response_message("Session does not exist"))
+
+    @staticmethod
+    def create_face_mask_info_from_contour_points(image: np.ndarray, face_till_nose_end_cpts: np.ndarray,
+                                                  left_eye_cpts: np.ndarray, right_eye_cpts: np.ndarray,
+                                                  left_eyebrow_cpts: np.ndarray, right_eyebrow_cpts: np.ndarray,
+                                                  mouth_without_lips_cpts: np.ndarray, nose_middle_point):
+        # Create masks of contour points.
+        image_shape = image.shape[:2]
+        face_mask = ImageUtils.get_face_mask_till_nose_end(image_shape, face_till_nose_end_cpts, left_eye_cpts,
+                                                           right_eye_cpts, left_eyebrow_cpts, right_eyebrow_cpts)
+        mouth_without_lips_mask = ImageUtils.get_mask_from_contour_points(image_shape, mouth_without_lips_cpts)
+        left_eye_mask = ImageUtils.get_mask_from_contour_points(image_shape, left_eye_cpts)
+        right_eye_mask = ImageUtils.get_mask_from_contour_points(image_shape, right_eye_cpts)
+
+        return FaceMaskInfo(face_mask_to_process=face_mask, mouth_mask_to_process=mouth_without_lips_mask,
+                            nose_middle_point=NoseMiddlePoint(x=nose_middle_point[0], y=nose_middle_point[1]),
+                            left_eye_mask=left_eye_mask, right_eye_mask=right_eye_mask)
 
     @staticmethod
     def create_face_mask_info(face_mask_to_process_rgb, mouth_mask_to_process_rgb, left_eye_mask_rgb,
